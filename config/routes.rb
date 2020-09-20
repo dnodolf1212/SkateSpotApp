2b7c7360b959bb49e1542572bb7262de4332f757
dnodolf1212 <dnodolf@gmail.com>
2020-09-20 13:48:19 -0700

    added controllers for users, skatespots and comments, using generators. Also generated corresponding routes (resources) and a root/home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
+   root 'home/users'
   resources :users
   resources :skatespots
+  resources :comments 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
