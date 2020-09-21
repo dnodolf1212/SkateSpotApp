637c46bbe5ae2e994ab2479a660011cc57cedbf1
dnodolf1212 <dnodolf@gmail.com>
2020-09-20 17:59:27 -0700

    set up associations between models, making COMMENTS a join tablee and SKATESPOT the has many through association.

--- app/models/skatespot.rb
+++ app/models/skatespot.rb
@@ -1,2 +1,5 @@
 class Skatespot < ApplicationRecord
+  belongs_to :user
+  has_many :comments, through: :users 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   root 'home/users'
+   root ''
   resources :users
   resources :skatespots
   resources :comments 
