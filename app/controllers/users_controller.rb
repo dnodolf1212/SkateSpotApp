8879e2575328ab7118e17011867db03308ccad5e
dnodolf1212 <dnodolf@gmail.com>
2020-09-24 12:52:50 -0700

    created sessions controller and began outlining forms/logic for signup and login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,16 @@ class UsersController < ApplicationController
   end
 
   def new 
+    
   end 
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to skatespot_path
+    else 
+      redirect_to login_path
+    end
   end 
 
   
