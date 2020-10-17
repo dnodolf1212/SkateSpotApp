2ff840effc15c3b1c20cd74c088b2892a63fb5db
dnodolf1212 <dnodolf@gmail.com>
2020-10-17 13:59:55 -0700

    link broke for skatespot index in render navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
   def render_navigation
     if session[:user_id]  
-      link_to "Logout", '/logout', method: 'delete' + " " + (link_to "All The Spots", skatespots_path)
-    else 
+      link_to "Logout", '/logout', method: 'delete' 
+    else
       link_to("Login", '/login') + " " + link_to("Sign Up", '/signup')
     end 
   end
